441fd24c9af279404ebf55f1546f9d0f9248c664
chuzonglin <4082848@qq.com>
2020-11-30 10:09:30 +0800

    1.增加admin页面

--- admin/src/router.js
+++ admin/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
-import Login from './views/login.vue'
 import Router from 'vue-router'
+import Login from './views/login.vue'
+import Admin from './views/admin.vue'
 
 Vue.use(Router);
 
@@ -9,9 +10,12 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [{
     path: '*',
-    redirect: "login",
+    redirect: "/login",
   },{
     path: '/login',
     component: Login
+  },{
+    path: '/admin',
+    component: Admin
   }]
 })
