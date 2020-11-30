e693263845ffba8f5e7112db279751f5df3caf8a
chuzonglin <4082848@qq.com>
2020-11-30 13:20:56 +0800

    1.增加business模块

---
+++ business/src/main/java/com/course/business/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.course.business.controller;
+
+import com.course.server.domain.Test;
+import com.course.server.mapper.TestMapper;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+public class TestController {
+
+    @Resource
+    private TestMapper testMapper;
+
+    @RequestMapping("/test")
+    public List<Test> test(){
+        return (List<Test>) testMapper.selectByExample(null);
+    }
+}
