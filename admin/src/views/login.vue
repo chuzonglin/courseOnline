214d855a86f8d4c79f3016727bcfe0da2d6dc368
chuzonglin <4082848@qq.com>
2020-11-30 10:13:17 +0800

    1.增加login跳转admin页面

--- admin/src/views/login.vue
+++ admin/src/views/login.vue
@@ -49,7 +49,8 @@
                             <span class="lbl"> 记住我</span>
                           </label>
 
-                          <button type="button" class="width-35 pull-right btn btn-sm btn-primary">
+                          <button type="button" class="width-35 pull-right btn btn-sm btn-primary"
+                              @click="login()">
                             <i class="ace-icon fa fa-key"></i>
                             <span class="bigger-110">登录</span>
                           </button>
@@ -76,6 +77,12 @@
 <script>
   $('body').attr('class', 'login-layout light-login');
   export default {
-    name: 'App',
+    name: 'login',
+    methods:{
+      login(){
+        this.$router.push("/admin")
+      }
+    }
   }
+
 </script>
